2e2f73760cb80cd926fbdb4c90fbc6cd0cf50bb5
Cristina Bubu <cristinaflorinamihalcea@hmail.com>
2024-01-22 06:12:44 +0200

    am adaugat iar tot

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,7 +8,9 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
+    function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
+    
+ 
 })();
